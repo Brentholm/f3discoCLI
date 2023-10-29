dc01399ab6ddcf859267124c70a471f905e45936
brentholm <holm.brent@gmail.com>
2023-10-28 19:42:38 -0500

    minor cleanup before adding custom commands

--- Core/Src/consoleIo.c
+++ Core/Src/consoleIo.c
@@ -6,11 +6,6 @@
 #include "usart.h"
 
 //use the windows conio.h for kbhit, or a POSIX reproduction
-#ifdef _WIN32
-#include <conio.h>
-#else
-#include "conioCompat.h"
-#endif
 
 //their original
 /*static int getch_noblock() {
@@ -20,51 +15,22 @@
         return EOF;
 }*/
 
-//my HAL version to do the same:
-//int getch_noblock(UART_HandleTypeDef *huart) {
-int getch_noblock(void) {
-    uint8_t receivedChar;
-
-    if (HAL_UART_Receive(&huart1, &receivedChar, 1, 1) == HAL_OK) {
-        //echo it out
-    	HAL_UART_Transmit(&huart1,&receivedChar, 1, 1);
-    	// A character has been received and stored in 'receivedChar'
-
-        return receivedChar;
-    } else {
-        // No character received within the timeout period
-        return 0xFFFFFFFF;  //this seems to match the EOF that ConsoleIOReceive is looking for.
-    }
-}
 
 eConsoleError ConsoleIoInit(void)
 {
 	return CONSOLE_SUCCESS;
 }
-/*eConsoleError ConsoleIoReceive(uint8_t *buffer, const uint32_t bufferLength, uint32_t *readLength)
-{
-	uint32_t i = 0;
-	char ch;
-	
-	ch = getch_noblock();
-	while ( ( EOF != (uint8_t)ch ) && ( i < bufferLength ) )
-	{
-		buffer[i] = (uint8_t) ch;
-		i++;
-		ch = getch_noblock();
-	}
-	*readLength = i;
-	return CONSOLE_SUCCESS;
-}*/
 
+//this is where the main edits went to make this code portable to STM32:
 eConsoleError ConsoleIoReceive(uint8_t* buffer, const uint32_t bufferLength, uint32_t *readLength)
 {
     uint32_t i = 0;
     uint8_t rxByte = 0;
 
-
+ //sit and spin here in case the UART is not yet ready
  while (HAL_UART_GetState(&huart1) != HAL_UART_STATE_READY);
 
+ //HAL_UART_Receive is blocking. It will sit and spin here for 2^32 or 4 billion ms, OR until a character arrives
  if (HAL_OK != HAL_UART_Receive(&huart1, &rxByte, 1, HAL_MAX_DELAY))
  {
   return CONSOLE_ERROR;
